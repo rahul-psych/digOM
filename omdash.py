2fe4e0920f8114782a336a53d12b04cb40f53e10
Rahul <56860506+rahul-psych@users.noreply.github.com>
2020-11-01 20:50:23 +1100

    shutdown

--- omdash.py
+++ omdash.py
@@ -8,8 +8,8 @@ import base64
 today = date.today()
 
 st.write('Upload file')
-uploaded_file = st.file_uploader("Choose a file")
-
+# uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = 'data.csv'
 st.title('Outcome Measures Report')
 cols_used = [2,3,32,33,57,58,59,60,61,71,77,86,93,111]
 # data = pd.read_csv("data.csv", usecols=cols_used, names=['URN', 'Date', 'Anxiety', 'Depression', 'PTSD', 'Intrusions', 'Avoidance', 'PTSD cognitions','Arousal', 'Insomnia', 'Alcohol', 'Anger', 'Function', 'Physical'], skiprows=1)
@@ -17,13 +17,19 @@ cols_used = [2,3,32,33,57,58,59,60,61,71,77,86,93,111]
 
 # Patient Selector
 if uploaded_file is not None:
+    # load data
     data = pd.read_csv(uploaded_file, usecols=cols_used, names=['URN', 'Date', 'Anxiety', 'Depression', 'PTSD', 'Intrusions', 'Avoidance', 'PTSD cognitions','Arousal', 'Insomnia', 'Alcohol', 'Anger', 'Function', 'Physical'], skiprows=1)
+    # change types
+    data['Date'] = data['Date'].astype('datetime64')
+    
     option = st.sidebar.selectbox(
         'For patient URN:',
         data['URN'].unique()
     )
+
     ptdata = data[data['URN'] == option]
     # # Convert dates into strings for use in charts
+    ptdata['DateStr'] =ptdata['Date'].dt.strftime('%d%m%Y')
     # dates = ptdata['Date']
     # li = dates.Date.tolist()
     # s = []
@@ -34,13 +40,15 @@ if uploaded_file is not None:
     st.write('### Report generated on: ' + str(today) + ' for URN: ' + str(option))
 
     st.write(ptdata)
+    st.write(ptdata.dtypes)
 
     st.write(ptdata['Date'])
 
     pclfig = px.bar(
         ptdata,
-        x=ptdata['Date'],
-        y='PTSD',
+        # x='Date',
+        x='DateStr',
+        y=['PTSD','Insomnia'],
         range_y=[0,80]
     )
     hadsfig = px.scatter_matrix(
@@ -49,7 +57,7 @@ if uploaded_file is not None:
         color=['Date']
     )
     st.plotly_chart(pclfig)
-    st.plotly_chart(hadsfig)
+    # st.plotly_chart(hadsfig)
 else:
     st.write('Upload data file to continue - export from redcap as CSV and tick "remove all tagged identifier fields"')
 
